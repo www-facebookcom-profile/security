673c9f82029a36663bf386d49598509d14e4d14f
www-facebookcom-profile <59163143+www-facebookcom-profile@users.noreply.github.com>
2019-12-29 11:02:00 +0800

    Add files via upload

---
+++ index_files/j_rZRU8rP9e.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["DcgC3"]); }
+
+__d("LoginIdentifySearchHintExperiment",["DOM","$"],(function a(b,c,d,e,f,g,h,i){function j(k,l,m,n,o,p,q,r,s){var t=true;function u(){var v=i(l),w=i(m),x=i(k);t=!t;h.setContent(v,t?p:q);h.setContent(x,t?n:o);w.setAttribute("placeholder",t?r:s)}h.listen(i(k),"click",null,u)}f.exports={setupLoginIdentifySearchToggle:j}}),null);
